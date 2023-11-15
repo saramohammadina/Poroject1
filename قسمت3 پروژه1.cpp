f16d5adf36ccd65fd342091da7ebdbc7837e0ecb
saramohammadina <139115701+saramohammadina@users.noreply.github.com>
2023-11-15 22:03:34 +0330

    Add files via upload

---
+++ قسمت3 پروژه1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void towersOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+    if (n == 1) {
+        cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << endl;
+        return;
+    }
+    towersOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
+    towersOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+
+int main() {
+    int n = 3; // number of disks
+    towersOfHanoi(n, 'A', 'C', 'B'); // A, B and C are the names of rods
+    return 0;
+}

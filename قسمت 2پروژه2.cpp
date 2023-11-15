c37d7cce61168bdb6b68218161ba1f83cc182266
saramohammadina <139115701+saramohammadina@users.noreply.github.com>
2023-11-15 22:59:24 +0330

    Update قسمت 2پروژه2.cpp

--- قسمت 2پروژه2.cpp
+++ قسمت 2پروژه2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 class Array {
 private:
@@ -14,26 +15,36 @@ public:
         delete[] arr;
     }
 
-    int& operator[](int index) {
-        return arr[index];
+    void Insert(int object, int index) {
+        arr[index] = object;
     }
 
-    int getSize() {
-        return size;
+    int Delete(int index) {
+        int object = arr[index];
+        arr[index] = 0;
+        return object;
+    }
+
+    int Find(int object) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == object) {
+                return i;
+            }
+        }
+        return -1;
     }
 };
 
 int main() {
     Array myArray(5);
+    myArray.Insert(10, 0);
+    myArray.Insert(20, 1);
+    myArray.Insert(30, 2);
+    myArray.Insert(40, 3);
+    myArray.Insert(50, 4);
 
-    for (int i = 0; i < myArray.getSize(); i++) {
-        myArray[i] = i + 1;
-    }
-
-    for (int i = 0; i < myArray.getSize(); i++) {
-        std::cout << myArray[i] << " ";
-    }
+    cout << "Element at index 2: " << myArray.Delete(2) << endl;
+    cout << "Index of element 40: " << myArray.Find(40) << endl;
 
     return 0;
 }
-        

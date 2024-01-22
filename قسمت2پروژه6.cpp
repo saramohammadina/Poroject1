6dc647e7101499ce8a35e7dfce99c02685915712
saramohammadina <139115701+saramohammadina@users.noreply.github.com>
2024-01-22 12:05:18 +0330

    Add files via upload

---
+++ قسمت2پروژه6.cpp
@@ -0,0 +1,9 @@
+class BinaryTree {
+public:
+    void Insert(int value);
+    void Remove(int value);
+    bool Search(int value);
+    std::string Preorder();
+    std::string Inorder();
+    std::string Postorder();
+};

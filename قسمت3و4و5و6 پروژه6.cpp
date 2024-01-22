bf6f67df91acb75ea34fa2a728beaacabbb0b973
saramohammadina <139115701+saramohammadina@users.noreply.github.com>
2024-01-22 12:05:39 +0330

    Add files via upload

---
+++ قسمت3و4و5و6 پروژه6.cpp
@@ -0,0 +1,17 @@
+AVLTree:
+Insert(value);
+Remove(value);
+
+
+RedBlackTree:
+Insert(value);
+Remove(value);
+
+MaxHeap, MinHeap:
+Insert(value);
+Remove(value);
+
+ 
+Tree:
+Insert(root, value);
+Remove(value);

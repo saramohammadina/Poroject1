e35dc623e1511b0d4711ac3d3bf0b726f98d90f8
saramohammadina <139115701+saramohammadina@users.noreply.github.com>
2024-01-22 12:06:03 +0330

    Add files via upload

---
+++ قسمت6پروژه6.cpp
@@ -0,0 +1,15 @@
+Graph:
+AddVertex(vertex);
+AddEdge(firstVertex, secondVertex);
+RemoveEdge(firstVertex, secondVertex);
+RemoveVertex(vertex);
+BFS(): return string;
+DFS(): return string;
+
+BubbleSort();
+SelectionSort();
+InsertionSort();
+MergeSort();
+QuickSort();
+BucketSort();
+CountingSort();
